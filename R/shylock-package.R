02cebcf71f3e7dc2e66eb1674c7a25799c405a73
John Kirkpatrick <john@Johns-Mac-Studio.local>
2026-07-04 06:35:54 +0100

    Remove dependency on data.table, add more CI checks

--- R/shylock-package.R
+++ R/shylock-package.R
@@ -3,15 +3,6 @@
 
 ## usethis namespace: start
 #' @import rlang
-#' @importFrom data.table :=
-#' @importFrom data.table .BY
-#' @importFrom data.table .EACHI
-#' @importFrom data.table .GRP
-#' @importFrom data.table .I
-#' @importFrom data.table .N
-#' @importFrom data.table .NGRP
-#' @importFrom data.table .SD
-#' @importFrom data.table data.table
 #' @importFrom glue glue
 #' @importFrom lifecycle deprecated
 #' @importFrom tibble tibble
